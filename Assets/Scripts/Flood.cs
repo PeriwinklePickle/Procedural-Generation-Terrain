836624c256262caaefe0eb9758fe6c602998d98b
PeriwinklePickle <erincarter0801@hotmail.com>
2017-11-02 09:52:42 -0700

    changing the map

--- Assets/Scripts/Flood.cs
+++ Assets/Scripts/Flood.cs
@@ -24,16 +24,20 @@ public class Flood : MonoBehaviour{
         // .. and increate the t interpolater
         t += 0.1f;
 
-        // now check if the interpolator has reached 1.0 AND if max == 1 to 
+        // now check if the interpolator has reached 1.0 AND if max > minimum to 
 		//only update map half the time 
         // and swap maximum and minimum so game object moves
         // in the opposite direction.
 		if (t > 1.0f)
         {
-			if (maximum == 1.0f) {
+			if (maximum > minimum) {
 				mapGenerator.seed = (int)Random.Range (-1000000f, 1000000f);
+				mapGenerator.noiseScale = (int)Random.Range (20f, 80f);
+				//TODO: random persisance and lacunarity, offset
 				mapGenerator.GenerateMap ();
 				print ("refresh \n");
+
+
 			}
             float temp = maximum;
             maximum = minimum;
